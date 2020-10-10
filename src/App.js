2c1e01850858462bc6c0ea4c706ecb3583f57905
Danny Ngo <danny@digitalgarden.com.au>
2020-10-10 18:21:25 +1100

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,  {useState, useEffect} from 'react';
 import './App.css';
+import Editor from './components/Editor';
 
 function App() {
+  const [html, setHtml] = useState('')
+  const [css, setCss] = useState('')
+  const [javascript, setJavaScript] = useState('')
+  const [scrDoc, setSrcDoc] = useState('')
+
+  useEffect(() => {
+
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+      <html>
+        <head>
+          <style>${css}</style>
+        </head>
+        <body>
+          ${html}
+          <script>${javascript}</script>
+        </body>
+      </html>
+      
+      `)
+    } , 250)
+
+    return () => { clearTimeout(timeout) }
+    
+  }, [html, css, javascript])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="editors-wrapper">
+          <Editor language='xml' displayName="HTML" value={html} onChange={setHtml} />
+          <Editor language='css' displayName="CSS" value={css} onChange={setCss} />
+          <Editor language='javascript' displayName="JS" value={javascript} onChange={setJavaScript} />
+      </div>
+      <div className="output-container"> 
+          <iframe
+            title="code-mirror"
+            sandbox="allow-scripts"
+            srcDoc={scrDoc}
+            width="100%"
+            height="100%"
+            frameBorder="0"
+          />
+      </div>
     </div>
   );
 }
